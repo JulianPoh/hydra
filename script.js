36d14f448c83aca9508d95fbd0995e0ea9a9b8a0
Julian Poh <julianpoh01@gmail.com>
2018-06-12 16:35:55 +0800

    changed code to use div instead of canvas

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.onload = function(){
 
+var gameWindow = document.getElementById("game-window");
 
 //Main Game Menu
     //Get playerName
@@ -32,32 +33,28 @@ window.onload = function(){
     //spawnSingleTarget Function
         //Create target & set random spawn Location
             var target
-            //Target Spawn XY Coordinates
+            var targetCounter = 0;
+            //Randomize x&y coordinates
             var x = Math.floor(Math.random() * (780 - 20 + 1)) + 10;
-            var y = Math.floor(Math.random() * (580- 20 + 1)) + 10;
+            var y = Math.floor(Math.random() * (580- 20 + 1)) + 10; 
             //Spawn Target Function
             function spawnTarget() {
-                target= gameCanvas.getContext('2d');
-                target.clearRect(0,0, 800,600);
-                target.beginPath();
-                target.fillStyle="red";
-                // Draws a circle of radius 20 at the coordinates x,y on the canvas
-                target.arc(x,y,20,0,Math.PI*2,true);
-                target.closePath();
-                target.fill();
-                target.addEventListener('click', function(event) {
-                // Collision detection between clicked offset and element.
-                targetDetection.forEach(function(target) {
-                if (y > element.top && y < element.top + element.height 
-                && x > element.left && x < element.left + element.width) {
-                
-                alert('clicked an element');
-                }
-            });
-        })
-    }  
-    //Call Functions
-    spawnTarget();
+                var newDiv = document.createElement('div');
+                newDiv.setAttribute("id", "target-div");
+                newDiv.style.width = 50;
+                newDiv.style.height = 50;
+                newDiv.style.background = "red";
+                newDiv.style.position = "absolute"
+                newDiv.style.left = x+'px';
+                newDiv.style.top = y+'px'; 
+                gameWindow.appendChild(newDiv);
+                newDiv.addEventListener('click', function() {
+                    targetCounter ++
+                    // gameWindow,removeChild(newDiv);
+                    })
+                };
+        //Call Functions
+        spawnTarget();
         //Set to despawn after x seconds
         //Add click eventlistener function
             //Despawns (Destroyed) on click
@@ -68,9 +65,9 @@ window.onload = function(){
         //gameState = true
         //Reset Target Destroyed Counter.
         //Start Timer
-        var thirtySecs = 30,
-            display = document.querySelector("#time");
-        startTimer(thirtySecs, display);
+        // var thirtySecs = 30,
+        //     display = document.querySelector("#time");
+        // startTimer(thirtySecs, display);
         //Spawn 1 target per second. End on timer.
         //Count Number of Spawns  & Calculate 90% mark rounded down to int.
         //Clicked targets >= 90% - playerScore ++
@@ -106,4 +103,7 @@ window.onload = function(){
 
 //Prize Redemption
 
+
+
 };
+
