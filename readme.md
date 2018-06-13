9349c08efe96085d1d487ea716c265d7097852fd
Julian Poh <julianpoh01@gmail.com>
2018-06-13 11:20:50 +0800

    random spawn location and color change working

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 DIFFUSE THE BOMBS
-- Bombs will appear in random locations change color after a few seconds (Green => Yellow => Red) and will "explode" (Game Over).
+- Bombs will appear in random locations change color after a few seconds (Green => Yellow => Orange => Red) and will "explode" (Game Over).
 - Players need to click on green bombs to remove them from game. As bombs change color, players need to click to change them back to the previous colors before removing them.
 
--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 window.onload = function(){
     //Declare game window
     var gameWindow = document.getElementById("game-window");
+    var gameState = false;
     //Set random x & y coordinates for game window.
     var x = Math.floor(Math.random() * (780 - 20 + 1)) + 10;
-    var y = Math.floor(Math.random() * (480- 20 + 1)) + 10; 
+    var y = Math.floor(Math.random() * (580 - 20 + 1)) + 10; 
 
     //Get playerName
     // var playerName = prompt("Please enter your name to start.");
@@ -25,31 +26,32 @@ window.onload = function(){
         }, 1000);
     }   
             
-            //Spawn Target Function
-            function spawnTarget() {
-                var newDiv = document.createElement('div');
-                newDiv.setAttribute("id", "target-div");
-                newDiv.style.position = "absolute"
-                newDiv.style.left = x+'px';
-                newDiv.style.top = y+'px'; 
-                //Create target in game window
-                gameWindow.appendChild(newDiv);
-                //Create cycling colors for target
-                // var colors = ["blue", "green", "yellow", "orange", "red"];
-                // for (var i=0; i<colors.length; i++) {
-                //     var colorChange = function() { 
-                //         newDiv.style.backgroundColor = colors[i];
-                //         var colorCycling = setInterval()
-                //     }   
-                // };
+    //Spawn Target Function
+    function spawnTarget() {
+        var newDiv = document.createElement('div');
+        newDiv.setAttribute("id", "target-div");
+        newDiv.style.position = "absolute"
+        newDiv.style.left = x+'px';
+        newDiv.style.top = y+'px';
+        newDiv.style.backgroundColor = "green";
+        //Change target Color from green-orange-red
+        setTimeout(function(){ newDiv.style.backgroundColor = "yellow"; }, 1500);
+        setTimeout(function(){ newDiv.style.backgroundColor = "orange"; }, 3000);
+        setTimeout(function(){ newDiv.style.backgroundColor = "red"; }, 4500);
+        setTimeout(function(){ newDiv.style.backgroundColor = "red"; }, 6000);
+         
+        //Create target in game window
+        gameWindow.appendChild(newDiv);
+    }
+    
+    
+              
+    spawnTarget();
 
-                
-        //Call Functions
-        spawnTarget();
-        //Set to despawn after x seconds
-        //Add click eventlistener function
-            //Despawns (Destroyed) on click
-            //Target Destroyed Counter ++
+    //Set to despawn after x seconds
+    //Add click eventlistener function
+    //Despawns (Destroyed) on click
+    //Target Destroyed Counter ++
 
     //Game Play
         //Instructions: click 90% to proceed.
