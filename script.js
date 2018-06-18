6daed4a54744387d290e37abe54f773028b35a53
Julian Poh <julianpoh01@gmail.com>
2018-06-18 13:24:59 +0800

    major overhaul to game file. Working game.

--- script.js
+++ script.js
@@ -1,106 +1,112 @@
-window.onload = function(){
-
-    setPlayerScore();
-    countdown()
-    spawnTarget();
 
 
-    
-
-};
-   
 //Declare global variables
 var gameWindow = document.getElementById("game-window");
-var gameState = false;
-var gameLevel = 1;
-// Random x & y coordinates for game window.
-var x = Math.floor(Math.random() * (800 - 0 + 1)) + 10;
-var y = Math.floor(Math.random() * (500 - 0 + 1)) + 10;
+var maxHydraHeads = 20;
+var playerScore = 0;
+var theScore = document.getElementById("score")
+theScore.innerHTML = playerScore;
 
-   
-function setPlayerScore() {
-    var playerScore = 0;
-    var gameScore = document.getElementById("score");
-    gameScore.innerHTML = playerScore;
+for (var i=0; i<maxHydraHeads; i++) {  //Generate Heads
+    var newHead = document.createElement("button");
+    newHead.setAttribute("class", "head");
+    newHead.style.width = "70px";
+    newHead.style.height = "70px";
+    newHead.style.borderRadius = "50%";
+    newHead.style.backgroundColor = "green";
+    newHead.style.backgroundImage = "url('img/head.png')";
+    newHead.style.position = "absolute";
+    var x = Math.floor(Math.random() * (730 - 70 + 1)) + 10;
+    newHead.style.left = x.toString() +'px';
+    var y = Math.floor(Math.random() * (430 - 70 + 1)) + 10;
+    newHead.style.top = y.toString() +'px';
+    newHead.addEventListener('click', modify);
+
+
+    newHead.style.display = "none";
+    gameWindow.appendChild(newHead);
 }
 
-//Game Timer Function
-var timeLeft = 30;
-var elem = document.getElementById('time');
-var timerId = setInterval(countdown, 1000);
+var heads = document.querySelectorAll("button");
+console.log(heads);
+var headsCounter = 0;
+var timer;
 
-function countdown() {
-    if (timeLeft == -1) {
-        clearTimeout(timerId);
-    } else {
-        elem.innerHTML = timeLeft;
-        timeLeft--;
-    }
-}
+function slowReveal() {
 
-//Spawn Target Function
-function spawnTarget() {
-    var newDiv = document.createElement('div'); 
-    newDiv.setAttribute("id", "target-div");
-    newDiv.style.position = "absolute";
-    newDiv.style.left = x+'px';
-    newDiv.style.top = y+'px';
-    newDiv.style.backgroundColor = "green";
-    newDiv.innerHTML = "'_'";
-
-//Spawn Multiple Targets
-function spawnMultipleTargets() {
-    for (var i=0; i<100; i++);
-        if (timeLeft > 0 && gameState == true) {
-            spawnTarget([i]);
+    function startCountDown() {
+        if (heads[headsCounter].style.backgroundColor == 'green') {
+            heads[headsCounter].style.backgroundColor = "yellow";
+        } else if (heads[headsCounter].style.backgroundColor == 'yellow') {
+            heads[headsCounter].style.backgroundColor = "orange";
+        } else if (heads[headsCounter].style.backgroundColor == 'orange') {
+            heads[headsCounter].style.backgroundColor = "red";
+        } else if (heads[headsCounter].style.backgroundColor == 'red') {
+            heads[headsCounter].style.display = "none";
+            gameWindow.style.backgroundColor = "red";
+            gameWindow.innerHTML = "GAME OVER";
+            gameState = false;
         }
-}
-var multiTargetsTimer = setInterval(spawnMultipleTargets, 1500);
+    }
 
-//Color Countdown till bomb explodes
-function bombCountdown() {
-    if (newDiv.style.backgroundColor == 'green') {
-        newDiv.style.backgroundColor = "yellow";
-        newDiv.innerHTML = "o_o";
-    } else if (newDiv.style.backgroundColor == 'yellow') {
-        newDiv.style.backgroundColor = "orange";
-        newDiv.innerHTML = "0_0";
-    } else if (newDiv.style.backgroundColor == 'orange') {
-        newDiv.style.backgroundColor = "red";
-        newDiv.innerHTML = "x_x";
-    } else if (newDiv.style.backgroundColor == 'red') {
-        alert("KABOOM!");
-        gameState = false;
+    if (headsCounter < maxHydraHeads){
+        heads[headsCounter].style.display = "inline-block";
+        headsCounter++;
+        timer = setInterval(startCountDown, 2000);
     }
 }
-var timer = setInterval(bombCountdown, 1500);
 
-//Function to change bomb color and diffuse bomb upon mouseclick
+var revTimer = setInterval(slowReveal, 1000);
+
+
 function modify() {
-    if (newDiv.style.backgroundColor == 'yellow') {
-        newDiv.style.backgroundColor = "green";
-        newDiv.innerHTML = "'_'";
-    } else if (newDiv.style.backgroundColor == 'orange') {
-        newDiv.style.backgroundColor = "yellow";
-        newDiv.innerHTML = "o_o";
-    } else if (newDiv.style.backgroundColor == 'red') {
-        newDiv.style.backgroundColor = "orange";
-        newDiv.innerHTML = "o_o";
-    } else if (newDiv.style.backgroundColor == 'green') {
+    if (this.style.backgroundColor == 'yellow') {
+        this.style.backgroundColor = "green";
+    } else if (this.style.backgroundColor == 'orange') {
+        this.style.backgroundColor = "yellow";
+    } else if (this.style.backgroundColor == 'red') {
+        this.style.backgroundColor = "orange";
+    } else if (this.style.backgroundColor == 'green') {
         clearInterval(timer);
-        removeBomb();
+        this.style.display = "none";
         playerScore++;
     }
 }
-addEventListener('click', modify)
-
-//Clear diffused bomb from game screen
-function removeBomb() {
-    gameWindow.removeChild(newDiv);
-}
-
-//Create target in game window
-    gameWindow.appendChild(newDiv);
-}
 
 
+
+
+
+
+
+
+
+// var gameState = false;
+// // Random x & y coordinates for game window.
+// // var x = Math.floor(Math.random() * (730 - 70 + 1)) + 10;
+// // var y = Math.floor(Math.random() * (430 - 70 + 1)) + 10;
+
+
+// var gameScore = document.getElementById("score");
+// gameScore.innerHTML = playerScore;
+
+
+// //Game Timer Function
+// var timeLeft = 30;
+// var elem = document.getElementById('time');
+// var timerId = setInterval(countdown, 1000);
+
+// function countdown() {
+//     if (timeLeft == -1) {
+//         clearTimeout(timerId);
+//     } else {
+//         elem.innerHTML = timeLeft;
+//         timeLeft--;
+//     }
+// }
+
+
+
+
+
+   
